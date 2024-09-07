dd38965dc8c266d2854a2ec3bbf17dbfffeca75b
Paweł Wlazło <pawel@wlazlo.dev>
2024-09-07 21:04:28 +0200

    moving stat component to stats.jsx

--- src/Stats.jsx
+++ src/Stats.jsx
@@ -1,3 +1,12 @@
+import Stat from "./Stat";
+
 export default function Stats() {
-  return <section className="stats">Stats</section>;
+  return (
+    <section className="stats">
+      <Stat count={0} title={"Words"} />
+      <Stat count={0} title={"Characters"} />
+      <Stat count={0} title={"Instagram"} />
+      <Stat count={0} title={"Twitter"} />
+    </section>
+  );
 }
