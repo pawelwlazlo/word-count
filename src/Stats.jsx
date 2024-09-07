fc102bd48913d006c98740436f6399fb7fce4f2f
Paweł Wlazło <pawel@wlazlo.dev>
2024-09-07 21:07:23 +0200

    moving Stat to Stats

--- src/Stats.jsx
+++ src/Stats.jsx
@@ -1,12 +1,19 @@
-import Stat from "./Stat";
-
 export default function Stats() {
   return (
     <section className="stats">
       <Stat count={0} title={"Words"} />
       <Stat count={0} title={"Characters"} />
-      <Stat count={0} title={"Instagram"} />
-      <Stat count={0} title={"Twitter"} />
+      <Stat count={280} title={"Instagram"} />
+      <Stat count={2200} title={"Twitter"} />
+    </section>
+  );
+}
+
+function Stat({ count, title }) {
+  return (
+    <section className="stat">
+      <span className="set__number">{count}</span>
+      <h2 className="second-heading">{title}</h2>
     </section>
   );
 }
