81bd727fd96c3787e2f4152faad31453a621adeb
Paweł Wlazło <pawel@wlazlo.dev>
2024-09-08 12:19:28 +0200

    moving components to components

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
+import { FACEBOOK_MAX_CHARACTERS, INSTAGRAM_MAX_CHARACTERS } from "../constants";
 import Stats from "./Stats";
 import Textarea from "./Textarea";
-import { FACEBOOK_MAX_CHARACTERS, INSTAGRAM_MAX_CHARACTERS } from "./constants";
 
 export default function Container() {
   const [text, setText] = useState("");
